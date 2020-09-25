068640b7402673f5125ec5268d82ab92598ba688
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-25 18:13:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -11,7 +11,7 @@ def run_guessing_game
     puts "Goodbye!"
   end 
   if user_input.to_i != random_number
-    puts "Sorry! The computer guessed #{random_number}."
+    puts "Sorry! The comuter guessed #{random_number}."
   end 
 end
 
